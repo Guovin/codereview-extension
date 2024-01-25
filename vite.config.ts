78a96179db638d5e1265aefdeb2cd49c4de03336
Govin <360996299@qq.com>
2024-01-25 14:52:36 +0800

    chore:build

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      imports: ['vue', 'vue/macros', VueRouterAutoImports]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup/index.html'),
+        background: resolve(__dirname, 'src/background/index.ts'),
+        'content-script': resolve(__dirname, 'src/content-script/index.ts')
+      },
+      output: {
+        entryFileNames: '[name].js'
+      }
+    }
+  }
+})
